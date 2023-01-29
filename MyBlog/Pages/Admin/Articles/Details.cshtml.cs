1d2b3ca42f4b2f743a994da9f88169c2672f2fec
Jingyu-An <jqrohan@gmail.com>
2023-01-29 11:21:50 -0500

    [MyBlog] Admin CRUD done

---
+++ MyBlog/Pages/Admin/Articles/Details.cshtml.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyBlog.Data;
+using MyBlog.Models;
+
+namespace MyBlog.Pages.Admin.Articles
+{
+    [Authorize(Roles = "Admin")]
+    public class DetailsModel : PageModel
+    {
+        private readonly BlogDbContext _context;
+
+        public DetailsModel(BlogDbContext context)
+        {
+            _context = context;
+        }
+
+      public Article Article { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Articles == null)
+            {
+                return NotFound();
+            }
+
+            var article = await _context.Articles.Include("Author").FirstOrDefaultAsync(m => m.Id == id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Article = article;
+            }
+            return Page();
+        }
+    }
+}
